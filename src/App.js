297d2825a77a3ad5ef94b54ad55f9fd0a3bfdd84
Gurunath Solanki <gurunathsolanki@gmail.com>
2022-10-06 15:35:21 +0100

    Added Modal using React Portals.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Cart from "./components/Cart/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const showCartHandler = () => {
+    setShowCart(true);
+  };
+
+  const closeCartHandler = () => {
+    setShowCart(false);
+  };
   return (
     <React.Fragment>
-      <Header />
+      {showCart && <Cart onClose={closeCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
