f2555e0b3f7b68d33383561d3a0b057b82ae5827
Gurunath Solanki <gurunathsolanki@gmail.com>
2022-10-10 18:05:47 +0100

    useRef & forwardRef for input elements.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [isAmountValid, setIsAmountValid] = useState(true);
+
+  const amountRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setIsAmountValid(false);
+      return;
+    }
+
+    setIsAmountValid(true);
+    props.onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +38,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!isAmountValid && <p>Please enter valid amount (1-5)</p>}
     </form>
   );
 };
