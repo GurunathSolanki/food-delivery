60ebbfd61dc987449152a3f1b8a1da81856a1f8c
Gurunath Solanki <gurunathsolanki@gmail.com>
2022-11-04 21:35:06 +0000

    Added error handling for REST calls.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,22 +6,34 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch(
       "https://react-http-93006-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
-    ).then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMeals = [];
-      for (const key in data) {
-        const meal = data[key];
-        transformedMeals.push({...meal, id: key})
-      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if(data === null) {
+          throw new Error("Something went wrong !!");
+        }
+        const transformedMeals = [];
+        for (const key in data) {
+          const meal = data[key];
+          transformedMeals.push({ ...meal, id: key });
+        }
 
-      setMeals(transformedMeals);
-      setIsLoading(false);
-    });
+        setMeals(transformedMeals);
+        setIsLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError("Something went wrong");
+        setIsLoading(false);
+        console.log(error);
+      });
   }, []);
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -35,8 +47,9 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        {isLoading && <p>Loading ...</p>}
-        {!isLoading && <ul>{mealsList}</ul>}
+        {isLoading && !error && <p>Loading ...</p>}
+        {!isLoading && !error && <ul>{mealsList}</ul>}
+        {error && <p>{error.toString()}</p>}
       </Card>
     </section>
   );
